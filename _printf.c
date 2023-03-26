06984224c478edfd273e18765637e9a6853ed978
nasergamal <naser.gamal98@gmail.com>
2023-03-26 16:28:04 +0200

    .

--- _printf.c
+++ _printf.c
@@ -46,7 +46,7 @@ int _printf(const char *format, ...)
  */
 int pri(const char *format, pr p[], va_list li)
 {
-	int i, c, n = 0, m = 0, a = 0;
+	int i, c, n = 0, m = 0;
 
 	for (i = 0; format && format[i]; i++)
 	{
@@ -58,11 +58,8 @@ int pri(const char *format, pr p[], va_list li)
 				{
 					m += p[c].func(li);
 					i++;
-					a = 1;
 					break;
 				}
-				else if (format[i + 1] == '\0' && a)
-					return (-1);
 				else if (format[i + 1] == '%')
 				{
 					i++;
